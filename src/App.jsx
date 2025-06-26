dd2349faa79c80707f812391294fc7c715567e3e
“Russel <russeldanielpaul@gmail.com>
2025-06-26 23:24:27 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
-    <>
-    <Landing />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GoogleAuth from '../components/GoogleAuth';
+
+const Signup = () => {
+  return <GoogleAuth />;
+};
+
+export default Signup;
